d0d7380a19a70dc7d747d92bd4b27832beca3786
Dave Krisopras Essanto <davemantul123456@gmail.com>
2025-06-13 18:39:52 +0700

    first commit

---
+++ frontend/src/pages/Error.js
@@ -0,0 +1,28 @@
+import { useRouteError } from "react-router-dom";
+import PageContent from "../components/PageContent";
+import MainNavigation from "../components/MainNavigation";
+
+export default function ErrorPage() {
+  const error = useRouteError();
+
+  let title = "An Error Occured!.";
+  let message = "Something went wrong!";
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+
+  if (error.status === 404) {
+    title = "Not Found";
+    message = "Could not find resources or page";
+  }
+
+  return (
+    <>
+    <MainNavigation />
+    <PageContent title={title}>
+      <p>{message}</p>
+    </PageContent>
+    </>
+  );
+}
